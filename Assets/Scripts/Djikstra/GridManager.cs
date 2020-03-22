13e3dc27f93fb39501ca45f1a08ac8f3f3d4b26d
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-03-22 11:40:07 +0700

    GridManagerScript done

---
+++ Assets/Scripts/Djikstra/GridManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+	[SerializeField]
+	private Vector2 worldSize;
+	[SerializeField]
+	private float gridSizeX;
+	[SerializeField]
+	private float gridSizeY;
+	public int gridNumberX { get; private set; }
+	public int gridNumberY { get; private set; }
+
+	public Vector2 GetWorldSize() {
+		return worldSize;
+	}
+
+	public float GetGridSizeX() {
+		return gridSizeX;
+	}
+
+	public float GetGridSizeY() {
+		return gridSizeY;
+	}
+
+	void Start() {
+		gridNumberX = Mathf.RoundToInt(worldSize.x / gridSizeX);
+		gridNumberY = Mathf.RoundToInt(worldSize.y / gridSizeY);
+	}
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position, new Vector3(worldSize.x, worldSize.y,1));
+	}
+}
--- Assets/Scripts/Djikstra/NodeManager.cs
+++ Assets/Scripts/Djikstra/NodeManager.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NodeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private LayerMask unwalkableMask;
+	[SerializeField]
+	private GridManager grid;
+	Node[,] nodes;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start() {
+		CreateGrid();
+	}
+
+	private void CreateGrid() {
+		nodes = new Node[grid.gridNumberX, grid.gridNumberY];
+
+	}
 }
