ff2e0747d21ec422d7335352b724d5c20de50ecb
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-03-22 20:19:36 +0700

    gridmanager and nodemanager done

--- Assets/Scripts/Djikstra/GridManager.cs
+++ Assets/Scripts/Djikstra/GridManager.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
 	[SerializeField]
+	private Transform gridArea;
+	[SerializeField]
 	private Vector2 worldSize;
 	[SerializeField]
 	private float gridSizeX;
 	[SerializeField]
 	private float gridSizeY;
+
 	public int gridNumberX { get; private set; }
 	public int gridNumberY { get; private set; }
+
+	public Vector2[,] gridPos { get; private set; }
 
 	public Vector2 GetWorldSize() {
 		return worldSize;
@@ -28,8 +33,27 @@ public class GridManager : MonoBehaviour
 	void Start() {
 		gridNumberX = Mathf.RoundToInt(worldSize.x / gridSizeX);
 		gridNumberY = Mathf.RoundToInt(worldSize.y / gridSizeY);
+
+
+		gridPos = new Vector2[gridNumberX, gridNumberY];
+		Vector2 WorldBottomLeft = Vector2.one *	(gridArea.position) - Vector2.right * (GetWorldSize().x/2f - GetGridSizeX()/2f) - Vector2.up * (GetWorldSize().y/2f - GetGridSizeY()/2f);
+		for(int i = 0; i < gridNumberX; i++) {
+			for(int j = 0; j < gridNumberY; j++) {
+				Vector2 worldPoint = WorldBottomLeft + Vector2.right * (i * GetGridSizeX()) + Vector2.up * (j * GetGridSizeY());
+				gridPos[i, j] = worldPoint;
+				//Debug.Log(gridPos[i, j]);
+			}
+		}
 	}
-	void OnDrawGizmos() {
-		Gizmos.DrawWireCube(transform.position, new Vector3(worldSize.x, worldSize.y,1));
-	}
+	//void OnDrawGizmos() {
+	//	Gizmos.DrawWireCube(transform.position, new Vector3(worldSize.x, worldSize.y,1));
+
+	//	if (gridPos != null) {
+	//		foreach (Vector2 v in gridPos) {
+	//			Debug.Log(v);
+	//			Gizmos.color = Color.gray;
+	//			Gizmos.DrawCube(Vector3.one * v, Vector3.one * new Vector2(gridSizeX - 0.1f, gridSizeY - 0.1f));
+	//		}
+	//	}
+	//}
 }
--- Assets/Scripts/Djikstra/NodeManager.cs
+++ Assets/Scripts/Djikstra/NodeManager.cs
@@ -9,14 +9,31 @@ public class NodeManager : MonoBehaviour
 	private LayerMask unwalkableMask;
 	[SerializeField]
 	private GridManager grid;
-	Node[,] nodes;
+	public Node[,] nodes { get; private set; }
 
 	void Start() {
-		CreateGrid();
+		CreateNodes();
 	}
 
-	private void CreateGrid() {
-		nodes = new Node[grid.gridNumberX, grid.gridNumberY];
+	private void CreateNodes() {
+		Vector2[,] gridPos = grid.gridPos;
+		nodes = new Node[gridPos.GetLength(0), gridPos.GetLength(1)];
 
+		for (int i = 0; i < gridPos.GetLength(0); i++) {
+			for(int j = 0; j < gridPos.GetLength(1); j++) {
+				RaycastHit2D cast = Physics2D.BoxCast(gridPos[i, j], new Vector2(grid.GetGridSizeX(), grid.GetGridSizeY()),0f,Vector2.zero,0f,unwalkableMask);
+				bool walkable = !(cast);
+				nodes[i, j] = new Node(walkable, gridPos[i, j]);
+			}
+		}
+	}
+
+	void OnDrawGizmos() {
+		if(nodes != null) {
+			foreach(Node n in nodes) {
+				Gizmos.color = (n.walkable) ? Color.green : Color.red;
+				Gizmos.DrawCube(n.worldPosition, new Vector3(grid.GetGridSizeX()-0.3f, grid.GetGridSizeY() - 0.3f, 1f));
+			}
+		}
 	}
 }
