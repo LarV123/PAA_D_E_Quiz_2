df8e2a6eeaa119a2646ffecb2dde284bd71baf87
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-03-22 10:04:12 +0700

    node class

---
+++ Assets/Scripts/Djikstra/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public bool walkable { get; }
+	public Vector2 worldPosition { get; }
+
+	public Node(bool _walkable, Vector2 _worldPos) {
+		walkable = _walkable;
+		worldPosition = _worldPos;
+	}
+}
