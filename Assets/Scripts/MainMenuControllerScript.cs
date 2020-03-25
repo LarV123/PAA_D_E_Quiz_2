d577a7de759e43d25ab422e7f1acdc533b72cf10
Imam Rafii Al Dzakwan <imamrafiia@gmail.com>
2020-03-25 21:42:43 +0700

    main menu

---
+++ Assets/Scripts/MainMenuControllerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControllerScript : MonoBehaviour
+{
+	public void PlayScene() {
+		SceneManager.LoadScene("Play");
+	}
+
+	public void ExitGame() {
+		Application.Quit();
+	}
+	
+}
